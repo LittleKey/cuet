f0d8c2e9e5b506c851538c5753fc13ff5a37c9d2
LittleKey <kunyu.liao@qianmo-inc.com>
2015-09-06 06:47:49 +0800

    code refactoring

--- src/CharDetermine.py
+++ src/CharDetermine.py
@@ -3,20 +3,20 @@
 
 class CharDetermine(object):
 
-    def __init__(self, encode_list):
-        self._encode_list_iter = iter(encode_list)
+    def __init__(self, decode_list):
+        self._decode_list_iter = iter(decode_list)
         self._nextEncode()
 
     def _nextEncode(self):
         try:
-            self._current_encode = self._encode_list_iter.next()
+            self._current_decode = self._decode_list_iter.next()
         except StopIteration:
-            raise IndexError("'{}' no more encode".format(self))
+            raise IndexError("'{}' no more decode".format(self))
 
     def processChar(self, chars):
         while (True):
             try:
-                return chars.decode(self._current_encode).encode('utf8')
+                return chars.decode(self._current_decode).encode('utf8')
             except UnicodeDecodeError:
                 self._nextEncode()
                 continue
