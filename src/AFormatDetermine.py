8b5bb25a508a7c2c36d3c308da866a5b1765ce6c
LittleKey <kunyu.liao@qianmo-inc.com>
2015-09-13 15:23:58 +0800

    fixed get format wrong

--- src/AFormatDetermine.py
+++ src/AFormatDetermine.py
@@ -3,11 +3,13 @@
 
 class AFormatDetermine(object):
 
+    # that support list
     _mapFormat2Encode = {
-            'wav': 'pcm_s16le'}
+            'wav': 'pcm_s16le',}
 
     _mapFormat2Extname = {
-            'wav': '.wav'}
+            'wav': '.wav',
+            'flac': '.flac'}
 
     def __init__(self, audioProcessor, defalt_format='wav'):
         self._ap = audioProcessor
@@ -17,8 +19,9 @@ class AFormatDetermine(object):
         return self._mapFormat2Encode.get(self.getFormat())
 
     def getFormat(self):
-        return AFormatDetermine._mapFormat2Encode.get(self._ap.getFormat(),
-                self._default_format)
+        if self._ap.getFormat() in AFormatDetermine._mapFormat2Encode.keys():
+            return self._ap.getFormat()
+        return self._default_format
 
     def getExtname(self):
         return AFormatDetermine._mapFormat2Extname.get(self.getFormat())
